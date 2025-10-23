e49504f4cfe2c0fd49d9e62223eeca60d8645e3b
Guilherme Duarte Silva Matos <up202208755@edu.fe.up.pt>
2025-10-23 17:32:45 +0100

    [CHG] New Backend + UFC arch

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,31 @@ mod grpc;
 mod pages;
 mod rest;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(Files::new("/", "frontend/out").index_file("index.html")))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+async fn run_user_facing_code() -> anyhow::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(Files::new("/", "frontend/out").index_file("index.html"))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await?;
+
+    Ok(())
+}
+
+async fn run_backend_code() -> anyhow::Result<()> {
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let local = tokio::task::LocalSet::new();
+    let ufc = local.run_until(async { tokio::task::spawn_local(run_user_facing_code()).await });
+
+    let backend = tokio::task::spawn(run_backend_code());
+
+    let handles = tokio::join!(ufc, backend);
+    handles.0??;
+    handles.1??;
+
+    Ok(())
 }
