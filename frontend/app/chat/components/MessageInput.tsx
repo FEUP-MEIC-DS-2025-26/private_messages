89602f6cb869d52d8899183ee277dfc5da1a320c
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-10-30 01:27:04 +0000

    [REF] Control emoji picker via React state

--- frontend/app/chat/components/MessageInput.tsx
+++ frontend/app/chat/components/MessageInput.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import Form from 'next/form';
-import EmojiPicker from 'emoji-picker-react';
-import { useRef } from 'react';
+import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
+import { useRef, useState } from 'react';
 
 // icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,14 +10,15 @@ import { faPaperPlane, faSmile } from '@fortawesome/free-solid-svg-icons';
 
 export default function MessageInput() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [showEmojis, setShowEmojis] = useState(false);
 
   // a function for handling emoji clicks
-  const handleEmojiClick = (emoji: string) => {
+  const handleEmojiClick = (emojiData: EmojiClickData) => {
     const input: HTMLInputElement | null = inputRef.current;
 
     // insert emoji at cursor position
     input?.setRangeText(
-      emoji,
+      emojiData.emoji,
       input.selectionStart ?? 0,
       input.selectionEnd ?? 0,
       'end',
@@ -25,40 +26,44 @@ export default function MessageInput() {
   };
 
   return (
-    <Form
-      action=""
-      className="sticky flex items-center gap-2 px-6 py-1 rounded-full border-2 focus-within:border-biloba-flower-500 transition-all"
-    >
-      {/** Input area */}
-      <input
-        ref={inputRef}
-        className="grow focus:outline-none focus:caret-biloba-flower-500 focus:caret-2"
-        name="message"
-        type="text"
-        placeholder="Type your message here"
-        required
-      />
-
-      {/** Button to toggle the emoji picker */}
-      <button
-        className="cursor-pointer hover:text-biloba-flower-500 transition-color"
-        type="button"
-        popoverTarget="emoji-picker"
-        popoverTargetAction="toggle"
+    <div className="relative">
+      <Form
+        action=""
+        className="sticky flex items-center gap-2 px-6 py-1 rounded-full border-2 focus-within:border-biloba-flower-500 transition-all"
       >
-        <FontAwesomeIcon icon={faSmile} />
-      </button>
-
-      <div id="emoji-picker" popover="auto">
-        <EmojiPicker
-          onEmojiClick={(emojiData) => handleEmojiClick(emojiData.emoji)}
+        {/** Input area */}
+        <input
+          ref={inputRef}
+          className="grow focus:outline-none focus:caret-biloba-flower-500 focus:caret-2"
+          name="message"
+          type="text"
+          placeholder="Type your message here"
+          required
         />
-      </div>
 
-      {/** Send button */}
-      <button className="w-6 h-6 cursor-pointer hover:text-biloba-flower-500 transition-color">
-        <FontAwesomeIcon icon={faPaperPlane} />
-      </button>
-    </Form>
+        {/** Button to toggle the emoji picker */}
+        <button
+          className="cursor-pointer hover:text-biloba-flower-500 transition-color"
+          type="button"
+          onClick={() => setShowEmojis(!showEmojis)}
+        >
+          <FontAwesomeIcon icon={faSmile} />
+        </button>
+
+        {/** Send button */}
+        <button className="w-6 h-6 cursor-pointer hover:text-biloba-flower-500 transition-color">
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </button>
+      </Form>
+
+      {/* Emoji picker */}
+      <div
+        id="emoji-picker"
+        className="absolute right-0 bottom-10 z-10"
+        hidden={!showEmojis}
+      >
+        <EmojiPicker onEmojiClick={handleEmojiClick} />
+      </div>
+    </div>
   );
 }
