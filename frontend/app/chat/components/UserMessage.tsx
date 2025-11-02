fd00fc39c55205d37cb040ff2fe4e4ee4925bad5
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-11-02 19:06:06 +0000

    [FIX] Display last conversation message

--- frontend/app/chat/components/UserMessage.tsx
+++ frontend/app/chat/components/UserMessage.tsx
@@ -15,7 +15,7 @@ export default function UserMessage({ isFromUser, content }: UserMessageProps) {
 
   return (
     <div
-      className={`w-fit max-w-3/4 p-3 rounded-3xl text-justify wrap-break-word ${extraStyles}`}
+      className={`w-fit max-w-4/5 p-3 rounded-3xl text-justify wrap-break-word ${extraStyles}`}
     >
       {content}
     </div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,51 +8,54 @@ const USERNAME = 'john';
 
 /**
  * A function for fetching the user's conversations from the server.
+ * @param {string} username - the user's username
  * @returns the user's conversations
  */
 const getConversations = async (username: string) => {
+  const API_URL = '/api/chat';
+
   // login
-  await fetch(`/api/chat/login?username=${username}`);
+  await fetch(`${API_URL}/login?username=${username}`);
 
   // fetch the conversations
-  const conversationIDs: number[] = await fetch('/api/chat/conversation').then(
+  const conversationIDs: number[] = await fetch(`${API_URL}/conversation`).then(
     (res) => res.json(),
   );
 
   // fetch the usernames of the users with whom we are conversing
   const usernames: string[] = await Promise.all(
     conversationIDs.map((id: number) =>
-      fetch(`/api/chat/conversation/${id}/peer`).then((res) => res.json()),
+      fetch(`${API_URL}/conversation/${id}/peer`).then((res) => res.json()),
     ),
   );
 
-  // fetch the user profiles
-  const userProfiles: { username: string; name: string }[] = await Promise.all(
+  // fetch the users' full names
+  const fullNames: string[] = await Promise.all(
     usernames.map((username: string) =>
-      fetch(`api/chat/user/${username}`).then((res) => res.json()),
+      fetch(`${API_URL}/user/${username}`)
+        .then((res) => res.json())
+        .then((user) => user.name),
     ),
   );
 
   // fetch the last message from each conversation
-  const lastMessage: string[] = await Promise.all(
+  const lastMessages: string[] = await Promise.all(
     conversationIDs.map((id: number) =>
-      fetch(`/api/chat/conversation/${id}/latest`).then((res) => res.json()),
+      fetch(`${API_URL}/conversation/${id}/latest`)
+        .then((res) => res.json())
+        .then((id: number) => fetch(`${API_URL}/message/${id}`))
+        .then((res) => res.json())
+        .then((message) => message.content.msg),
     ),
   );
 
   // create an array with the conversations
-  const conversations: any[] = [];
-
-  for (let index = 0; index < conversationIDs.length; ++index) {
-    conversations.push({
-      id: conversationIDs[index],
-      username: usernames[index],
-      name: userProfiles[index].name,
-      lastMessage: lastMessage[index],
-    });
-  }
-
-  return conversations;
+  return conversationIDs.map((id: number, index: number) => ({
+    id,
+    username: usernames[index],
+    name: fullNames[index],
+    lastMessage: lastMessages[index],
+  }));
 };
 
 /**
