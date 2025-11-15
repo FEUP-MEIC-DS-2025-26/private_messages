62ac1f562f3a7cf008ee6f48901cd24051aa1096
jvdcf <up202208393@up.pt>
2025-11-15 01:49:30 +0000

    [ADD] Terraform support for two Cloud Runs with BACKEND_URL env in frontend

--- terraform.tf
+++ terraform.tf
@@ -13,14 +13,14 @@ provider "google" {
   zone = "europe-southwest1-b"
 }
 
-resource "google_cloud_run_v2_service" "default" {
-  name     = "ds-2025-g51-private-messages"
+resource "google_cloud_run_v2_service" "backend" {
+  name     = "backend-private-messages"
   location = "europe-southwest1"
   deletion_protection = false
 
   template {
     containers {
-      image = "europe-southwest1-docker.pkg.dev/ds-2025-g51-private-messages/prototype/production"
+      image = "europe-southwest1-docker.pkg.dev/ds-2025-g51-private-messages/backend/production"
       ports {
         container_port = 8080
       }
@@ -28,16 +28,50 @@ resource "google_cloud_run_v2_service" "default" {
   }
 }
 
-resource "google_cloud_run_v2_service_iam_binding" "public_access" {
-  project  = google_cloud_run_v2_service.default.project
-  location = google_cloud_run_v2_service.default.location
-  name     = google_cloud_run_v2_service.default.name
+resource "google_cloud_run_v2_service_iam_binding" "backend_public_access" {
+  project  = google_cloud_run_v2_service.backend.project
+  location = google_cloud_run_v2_service.backend.location
+  name     = google_cloud_run_v2_service.backend.name
 
   role   = "roles/run.invoker"
   members = ["allUsers"]
 }
 
-output "service_url" {
-  description = "The URL of the deployed Cloud Run service."
-  value       = google_cloud_run_v2_service.default.uri
+output "backend_url" {
+  description = "The URL of the deployed backend Cloud Run service."
+  value       = google_cloud_run_v2_service.backend.uri
+}
+
+
+resource "google_cloud_run_v2_service" "frontend" {
+  name     = "frontend-private-messages"
+  location = "europe-southwest1"
+  deletion_protection = false
+
+  template {
+    containers {
+      image = "europe-southwest1-docker.pkg.dev/ds-2025-g51-private-messages/frontend/production"
+      ports {
+        container_port = 3001
+      }
+      env {
+        name  = "BACKEND_URL"
+        value = google_cloud_run_v2_service.backend.uri
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_binding" "frontend_public_access" {
+  project  = google_cloud_run_v2_service.frontend.project
+  location = google_cloud_run_v2_service.frontend.location
+  name     = google_cloud_run_v2_service.frontend.name
+
+  role   = "roles/run.invoker"
+  members = ["allUsers"]
+}
+
+output "frontend_url" {
+  description = "The URL of the deployed frontend Cloud Run service."
+  value       = google_cloud_run_v2_service.frontend.uri
 }
