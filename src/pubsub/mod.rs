4e85e80548c69aa78748dcd0bf491f70b57bda4e
Guilherme Duarte Silva Matos <up202208755@edu.fe.up.pt>
2025-11-23 17:37:42 +0000

    [ADD] easy macro for maintenance

--- src/pubsub/mod.rs
+++ src/pubsub/mod.rs
@@ -1,3 +1,28 @@
+macro_rules! include_protos {
+    ($package:literal,$version:literal) => {
+        include! {
+            concat!(
+                env!("OUT_DIR"),
+                "/",
+                $package,
+                ".",
+                $version,
+                ".rs"
+            )
+        }
+    };
+}
+
 pub mod priv_msgs_v1 {
-    include!(concat!(env!("OUT_DIR"), "/priv_msgs.v1.rs"));
+    include_protos!("priv_msgs", "v1");
+}
+
+#[cfg(test)]
+mod test {
+    use super::priv_msgs_v1::NewMessage;
+    #[test]
+    fn is_included() {
+        let msg = NewMessage::default();
+        println!("{msg:?}");
+    }
 }
