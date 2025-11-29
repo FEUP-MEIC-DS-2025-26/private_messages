1196831b711daa3529ad3a7e4cd23986b0581e7b
Pedro Gorobey <up202210292@up.pt>
2025-11-29 16:52:23 +0000

    Jumpseller: Improved error handling

--- src/jumpseller/mod.rs
+++ src/jumpseller/mod.rs
@@ -25,6 +25,23 @@ struct ProductWrapper {
     product: Product,
 }
 
+#[derive(Debug)]
+pub enum JumpSellerErr {
+    /// error occured before getting response (e.g. jumpseller unavailable)
+    RequestErr(reqwest::Error),
+
+    /// error occured when processing response (e.g. product not found when calling get_product())
+    ResponseErr(anyhow::Error),
+}
+
+// impl std::fmt::Display for JumpSellerErr {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "{:?}", self)
+//     }
+// }
+
+// impl std::error::Error for JumpSellerErr {}
+
 impl Client {
     pub fn new(login: String, token: String) -> Self {
         Self {
@@ -34,27 +51,32 @@ impl Client {
         }
     }
 
-    pub async fn get_product(&self, id: u64) -> anyhow::Result<Product> {
+    pub async fn get_product(&self, id: u64) -> anyhow::Result<Product, JumpSellerErr> {
         let response = self.client
             .get(format!("https://api.jumpseller.com/v1/products/{id}.json"))
             .basic_auth(&self.login, Some(&self.token))
             .send()
-            .await?;
+            .await
+            .or_else(|err| Err(JumpSellerErr::RequestErr(err)))
+            ?;
 
         let status = response.status();
-        let body = response.text().await?;
+        let body = response.text().await.or_else(|err| Err(JumpSellerErr::ResponseErr(err.into())))?;
 
         if status != 200 {
-            let json: serde_json::Value = serde_json::from_str(&body)?;
+            let json: serde_json::Value = serde_json::from_str(&body).or_else(|err| Err(JumpSellerErr::ResponseErr(err.into())))?;
             let err_str = json
-                .get("error")
+                .get("message")
                 .and_then(|v| v.as_str())
                 .unwrap_or("Request failed")
                 ;
-            return Err(anyhow!(err_str.to_owned()));
+            return Err(JumpSellerErr::ResponseErr(anyhow!(err_str.to_owned())));
         }
 
-        let product = serde_json::from_str::<ProductWrapper>(&body)?.product;
+        let product = serde_json::from_str::<ProductWrapper>(&body)
+            .or_else(|err| Err(JumpSellerErr::ResponseErr(err.into())))?
+            .product;
+
         Ok(product)
     }
 }
