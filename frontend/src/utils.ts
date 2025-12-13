8176586b5731deae847af89e76db7dba69d4df23
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-12-13 17:16:31 +0000

    [REF] Refactor data fetcher

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,4 +1,33 @@
 /**
+ * A function for fetching data from the backend.
+ * @param {string} URL - the URL to fetch data from
+ */
+export const fetcher = async (URL: string) => {
+  const response = await fetch(URL, { credentials: 'include' });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error(await response.text());
+};
+
+/**
+ * A function for logging the user into the system.
+ * @param URL - the backend URL
+ * @param userID - the user's JumpSeller ID
+ */
+export const login = async (URL: string, userID: number) => {
+  const response = await fetch(`${URL}/login?id=${userID}`, {
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error(await response.text());
+  }
+};
+
+/**
  * Formats a date as a string.
  * @param date - the date to format
  * @returns a string representing the date
@@ -28,21 +57,3 @@ export function formatDate(date: Date): string {
 
   return components.join(' ');
 }
-
-/**
- * A function for fetching data from the backend.
- * @param {string} URL - the URL
- */
-export const fetcher = (URL: string) =>
-  fetch(URL, { credentials: 'include' }).then(async (res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error(await res.text());
-  });
-
-export const login = async (URL: string, userID: number) => await fetch(`${URL}/login?id=${userID}`, { credentials: 'include' }).then(async (res) => {
-    if (!res.ok) {
-      throw new Error(await res.text());
-    }
-  });
